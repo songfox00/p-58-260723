ae2e73949a04cce42dfaf2aacdfb039d8900bd3a
songfox00 <songfox00@gmail.com>
2026-07-23 11:10:40 +0900

    번호 증가 r,g

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -11,21 +11,23 @@ public class App {
     public void run(){
         System.out.println("== 명언 앱 ==");
 
-        System.out.print("명령) ");
-        String cmd = sc.nextLine();
+        while (true) {
+            System.out.print("명령) ");
+            String cmd = sc.nextLine();
 
-        switch (cmd) {
-            case "등록" -> {
-                System.out.print("명언 : ");
-                String saying = sc.nextLine();
-                System.out.print("작가 : ");
-                String author = sc.nextLine();
+            switch (cmd) {
+                case "등록" -> {
+                    System.out.print("명언 : ");
+                    String saying = sc.nextLine();
+                    System.out.print("작가 : ");
+                    String author = sc.nextLine();
 
-                lastId++;
-                System.out.println("%d번 명언이 등록되었습니다." .formatted(lastId));
-            }
-            case "종료" -> {
-                return;
+                    lastId++;
+                    System.out.println("%d번 명언이 등록되었습니다.".formatted(lastId));
+                }
+                case "종료" -> {
+                    return;
+                }
             }
         }
     }
