45e52ddd9d6898a481f6d33417456803906e6e00
songfox00 <songfox00@gmail.com>
2026-07-23 09:40:31 +0900

    exp2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,38 @@
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        experiment1();
+//        experiment1();
+        experiment2();
+    }
+
+    public static void experiment2() {
+        System.out.println("안녕하세요");
+
+        PrintStream originalOut = System.out;   //모니터
+
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();   //배열
+        PrintStream printStream = new PrintStream(outputStream);    //배열로 가는 통로
+
+        System.setOut(printStream); //목적지를 배열로
+
+        System.out.print("하하하");  //콘솔에 출력x
+
+        String outStr = outputStream.toString();    //배열에 저장된 값 저장
+        System.setOut(originalOut); //원래 모니터로 돌리기
+        printStream.close();
+
+        if(outStr.equals("하하하")) {
+            System.out.println("출력 결과가 일치합니다.");
+        } else {
+            System.out.println("출력 결과가 일치하지 않습니다.");
+        }
+
+        System.out.println(outStr);
+
+
     }
 
     public static void experiment1() {
