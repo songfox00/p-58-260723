5427a206f6188b8fb9b803196d3d5b1acdfca91b
songfox00 <songfox00@gmail.com>
2026-07-23 12:37:32 +0900

    overloading 예제

---
+++ src/test/java/OverloadingTest.java
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.Test;
+
+public class OverloadingTest {
+    @Test
+    void t1() {
+        Person p1 = new Person();
+        p1.introduce();
+        p1.introduce(10);
+        p1.introduce("홍길동");
+        p1.introduce(20);
+
+        Person p2 = new Person();
+        Person p3 = new Person(20, "홍길동");
+    }
+}
