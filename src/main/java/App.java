bffa0830efe3c7c2b31f11d39b5ee015987e2d83
songfox00 <songfox00@gmail.com>
2026-07-23 11:16:28 +0900

    목록 r,g

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,8 +1,11 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class App {
     private Scanner sc;
     private int lastId=0;
+    List<WiseSaying> wiseSayings = new ArrayList<>();
 
     public App(Scanner sc) {
         this.sc=sc;
@@ -23,7 +26,20 @@ public class App {
                     String author = sc.nextLine();
 
                     lastId++;
+                    WiseSaying wiseSaying = new WiseSaying(lastId, saying, author);
+                    wiseSayings.add(wiseSaying);
+
                     System.out.println("%d번 명언이 등록되었습니다.".formatted(lastId));
+                }
+                case "목록" ->{
+                    System.out.println("번호 / 작가 / 명언");
+                    System.out.println("----------------------");
+
+                    wiseSayings
+                            .reversed()
+                            .stream()
+                            .forEach(wiseSaying -> System.out.printf("%d / %s / %s%n",
+                                    wiseSaying.getId(), wiseSaying.getAuthor(), wiseSaying.getSaying()));
                 }
                 case "종료" -> {
                     return;
