2c7e01f13f6fda4975ca52e3789365bd89ecbb08
songfox00 <songfox00@gmail.com>
2026-07-23 10:40:18 +0900

    AppTestRunner, 등록 r,g

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -1,9 +1,6 @@
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.io.ByteArrayOutputStream;
-import java.util.Scanner;
-
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class AppTest {
@@ -11,14 +8,24 @@ public class AppTest {
     @Test
     @DisplayName("'== 명언 앱 ==' 출력")
     void t1() {
-        Scanner sc = TestUtil.genScanner("종료");
 
-        ByteArrayOutputStream outputStream = TestUtil.setOutToByteArray();
-        new App(sc).run();
-        String out = outputStream.toString();
+        String out = AppTestRunner.run("");
 
         assertThat(out).contains("== 명언 앱 ==");
-
     }
 
+    @Test
+    @DisplayName("등록")
+    void t2() {
+
+        String out = AppTestRunner.run("""
+                등록
+                현재를 사랑하라.
+                작자미상
+                """);
+
+        assertThat(out).contains("명령) ");
+        assertThat(out).contains("명언 : ");
+        assertThat(out).contains("작가 : ");
+    }
 }
