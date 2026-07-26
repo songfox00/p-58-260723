6073d5d0bb6f9f1f2ec5218a26b2a203f12cedf2
songfox00 <songfox00@gmail.com>
2026-07-26 22:52:57 +0900

    file touch, exists

---
+++ src/test/java/util/UtilFileTest.java
@@ -0,0 +1,25 @@
+package util;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class UtilFileTest {
+    @Test
+    @DisplayName("파일 생성")
+    void t1() {
+
+        // 무언가를 세팅하고 (given)
+        String filePath = "test.txt";
+
+        // 수행하면 (when)
+        Util.file.touch(filePath);
+
+        // 결과가 나온다. => 실제 파일이 존재하는가? (then)
+        boolean rst = Util.file.exists(filePath);
+
+        assertThat(rst).isTrue();
+
+    }
+}
