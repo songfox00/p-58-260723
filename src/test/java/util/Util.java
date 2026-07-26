c95cf04209f5f125de218ae40633ef2023609ad7
songfox00 <songfox00@gmail.com>
2026-07-26 22:54:05 +0900

    file delete

--- src/test/java/util/Util.java
+++ src/test/java/util/Util.java
@@ -1,10 +1,8 @@
 package util;
 
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.nio.file.StandardOpenOption;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
 
 public class Util {
     // 이너 클래스
@@ -49,6 +47,30 @@ public class Util {
 
         public static boolean exists(String filePath) {
             return Files.exists(getPath(filePath));
+        }
+
+        private static class FileDeleteVisitor extends SimpleFileVisitor<Path> {
+            @Override
+            public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                Files.delete(file);
+                return FileVisitResult.CONTINUE;
+            }
+
+            @Override
+            public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+                Files.delete(dir);
+                return FileVisitResult.CONTINUE;
+            }
+        }
+
+
+        public static boolean delete(String filePath) {
+            try {
+                Files.walkFileTree(getPath(filePath), new FileDeleteVisitor());
+                return true;
+            } catch (IOException e) {
+                return false;
+            }
         }
     }
 }
