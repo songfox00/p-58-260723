9bca5bac4e5e0fc486e0dfe5b5e5e7eb2f8ddddd
songfox00 <songfox00@gmail.com>
2026-07-26 22:39:13 +0900

    dup refactoring

--- src/main/java/com/back/domain/wiseSaying/repository/WiseSayingRepository.java
+++ src/main/java/com/back/domain/wiseSaying/repository/WiseSayingRepository.java
@@ -38,25 +38,15 @@ public class WiseSayingRepository {
 
     public PageDto findByContentContainingIdDesc(String keyword, int pageSize, int page) {
 
-        // 페이징 처리 된 결과 + 페이징 메타 정보
-        // 현재 페이지 번호 + 페이지 사이즈 + 전체 페이지 개수 + 시작 페이지 번호 + 마지막 페이지 번호
-
-        // 명언 목록 + 페이지 번호 + 전체 페이지 개수\
-
-        // 여러개의 값을 하나로 다루는 법 -> 배열, 클래스
         List<WiseSaying> result = wiseSayings
                 .reversed()
                 .stream()
                 .filter(
                         w -> w.getContent().contains(keyword)
                 )
-                .skip((page - 1) * pageSize)
-                .limit(pageSize)
                 .toList();
 
-        int totalItems = wiseSayings.size();
-
-        return new PageDto(page, pageSize, totalItems, result);
+        return pageOf(result, page, pageSize);
     }
 
     public PageDto findByAuthorContainingIdDesc(String keyword, int pageSize, int page) {
@@ -66,11 +56,21 @@ public class WiseSayingRepository {
                 .filter(
                         w -> w.getAuthor().contains(keyword)
                 )
-                .skip((page - 1) * pageSize)
                 .limit(pageSize)
                 .toList();
-        int totalItems = wiseSayings.size();
 
-        return new PageDto(page, pageSize, totalItems, result);
+        return pageOf(result, page, pageSize);
+    }
+
+    private PageDto pageOf(List<WiseSaying> filteredContent, int pageNo, int pageSize) {
+
+        List<WiseSaying> content = filteredContent.stream()
+                .skip((pageNo-1) * pageSize)
+                .limit(pageSize)
+                .toList();
+
+        int totalItems = filteredContent.size();
+
+        return new PageDto(pageNo, pageSize, totalItems, content);
     }
 }
