64da25bcf5135dd1984007e13c36ad2ca5775073
songfox00 <songfox00@gmail.com>
2026-07-26 22:18:02 +0900

    lombok add

--- src/main/java/com/back/domain/wiseSaying/entity/WiseSaying.java
+++ src/main/java/com/back/domain/wiseSaying/entity/WiseSaying.java
@@ -1,5 +1,10 @@
 package com.back.domain.wiseSaying.entity;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
 public class WiseSaying {
     private int id;
     private String saying;
@@ -7,30 +12,6 @@ public class WiseSaying {
 
     public WiseSaying(String saying, String author) {
         this.saying = saying;
-        this.author = author;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getSaying() {
-        return saying;
-    }
-
-    public void setSaying(String saying) {
-        this.saying = saying;
-    }
-
-    public String getAuthor() {
-        return author;
-    }
-
-    public void setAuthor(String author) {
         this.author = author;
     }
 
